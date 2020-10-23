36b728fad089c1a60f2560b368ea0dbd30a203a0
aggfrassica <58398828+aggfrassica@users.noreply.github.com>
2020-10-23 14:34:15 +0200

    Add files via upload
    FirstCommit

---
+++ LivianaWEB/Web/navbar.php
@@ -0,0 +1,18 @@
+<?php
+
+echo '
+<header class="sopra">
+	<nav>
+    <h1 id="logo"><a href="index.php" title="homepage">Nome<span>Sito</span></a></h1>
+    <ul>
+      <li><a href="index.php" title="homepage">Home</a></li>
+      <li><a href="vetrina.php" title="vetrina">Abbonamento</a></li>
+       <li><a href="vetrina.php" title="vetrina">I nostri siti</a></li>
+       <li><a href="about.php" title="contatti">Contattaci</a></li>
+    </ul>
+  </nav>
+</header>
+';
+
+
+?>
